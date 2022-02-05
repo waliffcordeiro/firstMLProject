4d4f44824298f48259f6f4974e88db32ceab8f61
Waliff Cordeiro <waliffcordeiro@gmail.com>
2022-02-05 17:24:42 -0300

    Cleanup

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from import_data import returnsDataSet
 from lib_versions import getLibVersions
 from summarize_dataset import summarizeDataset
 from show_graphs import showGraphs
-from evaluate_algorithms import createValidationDataset, evaluateModels
+from evaluate_algorithms import createValidationDataset, evaluateModels, algorithmComparison
 
 
 
@@ -12,5 +12,6 @@ if __name__ == '__main__':
     _, names, dataset = returnsDataSet()
     summarizeDataset(dataset)
     showGraphs(dataset)
-    X_train, _, Y_train, _ = createValidationDataset(dataset)
-    evaluateModels(X_train, Y_train)
+    X_train, X_validation, Y_train, Y_validation = createValidationDataset(dataset)
+    algorithmComparison(X_train, Y_train)
+    evaluateModels(X_train, Y_train, X_validation, Y_validation)
